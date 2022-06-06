0b75bdfeaaf1f30a2c36da58f3684e2ed2d7657c
Paul Vilain <paul.vilain@etu.utc.fr>
2022-06-06 22:18:22 +0200

    add date_page 2

--- screens/Date2.tsx
+++ screens/Date2.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import { useNavigation } from '@react-navigation/native';
-import { StyleSheet} from 'react-native';
+import {Image, StyleSheet} from 'react-native';
 import type { StackNavigationProp } from '@react-navigation/stack';
-import { Text, View } from '../components/Themed';
-import Calendar from "../components/Calendar"
+import { View } from '../components/Themed';
 import Title from "../components/Title";
 import Subtitle from "../components/Subtitle";
-
 
 
 export default function Date() {
@@ -15,10 +13,7 @@ export default function Date() {
     <View style={styles.container}>
       <Title text='Résultats des dates'/>
       <Subtitle text="Tout le monde a voté, le calendrier ci-dessous présente les résultats."/>
-      <div style={{position: 'absolute', top: '200px'}}>
-        <button style={{width: '112px', height: '32px', backgroundColor: '#1EA21C', borderRadius: 16}}>Disponible</button>
-        <button style={{width: '112px', height: '32px', backgroundColor: '#E9A16D', borderRadius: 16}}>Peut-Être</button>
-      </div>
+      <Image style={{width: 388, height: 492, resizeMode: "center", overflow: "visible"}} source = {require('../Calendar.png')} />
     </View>
   );
 }
@@ -41,5 +36,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#1EA21C',
     // left: calc(50% - 225/2 - 2),
     // top: calc(50% - 32/2 - 123)
+  },
+  image: {
+    width: '486px',
+    height: '616px',
+    resizeMode: 'stretch'
   }
 });
